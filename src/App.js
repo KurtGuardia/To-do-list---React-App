31e1b8b75f49d0a7a73b4737368ca485c1f27845
Kurt Guardia <kurtguardia@gmail.com>
2020-04-02 01:30:27 +0200

    Set up React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import Todos from './Components/Todos';
 import './App.css';
+import Todos from './Components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Study React',
+        compleated: false
+      },
+      {
+        id: 2,
+        title: 'Do Business Plan',
+        compleated: false
+      },
+      {
+        id: 3,
+        title: 'Make JS project',
+        compleated: false
+      },
+    ]
+  }
+
+  render(){  
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
